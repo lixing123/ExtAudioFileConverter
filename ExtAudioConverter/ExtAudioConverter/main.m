a12eb09ff3830e18ae0d3e13bcb808312b23fb8d
Xing Li <shangwangwanwan@gmail.com>
2015-04-23 18:11:45 +0800

    No changes

--- ExtAudioConverter/ExtAudioConverter/main.m
+++ ExtAudioConverter/ExtAudioConverter/main.m
@@ -13,17 +13,18 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         ExtAudioConverter* converter = [[ExtAudioConverter alloc] init];
         converter.inputFile =  @"/Users/lixing/Desktop/playAndRecord.caf";
-        converter.inputFile =  @"/Users/xingli/Desktop/input.mp3";
+        //converter.inputFile =  @"/Users/lixing/Desktop/new.m4a";
+        //converter.inputFile =  @"/Users/xingli/Desktop/input.mp3";
         //output file extension is for your convenience
-        converter.outputFile = @"/Users/xingli/Desktop/output.wav";
+        converter.outputFile = @"/Users/lixing/Desktop/output.mp3";
         
         //TODO:some option combinations are not valid.
         //Check them out
         converter.outputSampleRate = 44100;
         converter.outputNumberChannels = 2;
         converter.outputBitDepth = BitDepth_16;
-        converter.outputFormatID = kAudioFormatLinearPCM;
-        converter.outputFileType = kAudioFileCAFType;
+        converter.outputFormatID = kAudioFormatMPEGLayer3;
+        converter.outputFileType = kAudioFileMP3Type;
         [converter convert];
     }
     return 0;
